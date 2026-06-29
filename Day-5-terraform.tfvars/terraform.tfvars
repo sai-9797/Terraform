d6a2e6f454598175045cc9f3f251f60a72861883
saikrishnaj123456-ux <saikrishnaj123456@gmail.com>
2026-06-29 21:08:16 +0530

    upgrades

---
+++ Day-5-terraform.tfvars/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-08f44e8eca9095668"
+instance_type = "t3.micro"
+name = "my-ec2-instance"
---
+++ Day-5-terraform.tfvars/variables.tf
@@ -0,0 +1,17 @@
+variable "ami_id" {
+    type = string
+    default = ""
+  
+}
+
+variable "instance_type" {
+    type = string
+    default = ""
+  
+}
+
+variable "name" {
+    type = string
+    default = ""
+  
+}
